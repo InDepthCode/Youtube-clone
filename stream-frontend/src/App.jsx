72f182d6345fd886a7eb3048d1a438e51196d409
InDepthCode <amrut.work@gmail.com>
2024-11-20 08:25:11 +0530

    added frontend

---
+++ stream-frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoUpload from './components/videoUpload'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex flex-col space-y-5 justify-center ">
+        <h1 className="text-4xl font-bold text-gray-700 dark:text-gray-100">
+          Hello world
+        </h1>
+
+        <VideoUpload />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ stream-frontend/src/components/VideoUpload.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VideoUpload() {
+  return (
+    <div className="text-white">VideoUpload</div>
+  )
+}
+
+export default VideoUpload;
